fbfb007c02289de90d4ff648edda662599dc82c4
lmaso <lmason98@gmail.com>
2018-11-29 18:18:10 -0800

    Linked SFML

--- asteroid_shooter/asteroid_shooter/main.cpp
+++ asteroid_shooter/asteroid_shooter/main.cpp
@@ -1,10 +1,27 @@
 #pragma once
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	RenderWindow win(VideoMode(1000, 600), "Asteroid Shooter!");
+
+	while (win.isOpen())
+	{
+		Event event;
+
+		while (win.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				win.close();
+		}
+
+		win.clear();
+		win.display();
+	}
 
 	return EXIT_SUCCESS;
 }
